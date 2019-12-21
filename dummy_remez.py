c92bb4fbaa73a42d2b62e67510fb0e8b24053fd1
Nicolas Brunie <nibrunie@gmail.com>
2019-12-21 10:38:07 +0100

    [refactor] refactoring code into functions

--- dummy_remez.py
+++ dummy_remez.py
@@ -5,20 +5,22 @@ import fpylll
 import numpy as np
 import matplotlib.pyplot as plt
 
-function = lambda x: bigfloat.tanh(x)
+#function = lambda x: bigfloat.exp(x)
 
 
-poly_degree = 8
-NUM_POINT = 10
-interval_lo = 1.0
-interval_hi = 1.05
+#poly_degree = 4
+#NUM_POINT = 100
+#interval_lo = 0.001
+#interval_hi = 0.5
 
-epsilon = 0.01
+#epsilon = 0.01
 
-interval_size = interval_hi - interval_lo
 
-def cheb_root(degree, index):
+#interval_size = interval_hi - interval_lo
+
+def cheb_root(degree, index, interval=(-1, 1)):
     """ Return the index root of a chebyshev polynomial of degree degree
+        linearly mapped onto interval
 
         :param degree: Chebyshev polynomial degree
         :type degree: int
@@ -27,9 +29,13 @@ def cheb_root(degree, index):
         :return: index-th root of the degree-th Chebyshev polynomial
         :retype: bigfloat.BigFloat
     """
-    return (interval_lo + interval_hi) / 2 + interval_size * 0.5 * bigfloat.cos(bigfloat.const_pi() * (index + 0.5) / bigfloat.BigFloat(degree))
-def cheb_extrema(degree, index):
-    """ Return the index-th extrream of a chebyshev polynomial of degree degree
+    lo, hi = interval
+    size = hi - lo
+    return (lo + hi) / 2 + size * 0.5 * bigfloat.cos(bigfloat.const_pi() * (index + 0.5) / bigfloat.BigFloat(degree))
+
+def cheb_extrema(degree, index, interval=(-1, 1)):
+    """ Return the index-th extrema of a chebyshev polynomial of degree degree
+        linearly mapped onto interval
 
         :param degree: Chebyshev polynomial degree
         :type degree: int
@@ -38,69 +44,79 @@ def cheb_extrema(degree, index):
         :return: index-th extrema of the degree-th Chebyshev polynomial
         :retype: bigfloat.BigFloat
     """
-    return (interval_lo + interval_hi) / 2 + interval_size * 0.5 * bigfloat.cos(bigfloat.const_pi() * index / bigfloat.BigFloat(degree))
+    lo, hi = interval
+    size = hi - lo
+    return (lo + hi) / 2 + size * 0.5 * bigfloat.cos(bigfloat.const_pi() * index / bigfloat.BigFloat(degree))
 
-def get_random_interval_pt():
-    """ generate a random point within [interval_lo, interval_hi]
+def get_random_interval_pt(interval):
+    """ generate a random point within interval (tuple (lo, hi))
 
-        :return: random value in [interval_lo, interval_hi]
+        :param interval: value where random value must be picked
+        :type interval: tuple(float, float)
+        :return: random value in [lo, hi]
         :rtype: float
     """
-    return interval_lo + interval_size * random.random()
+    lo, hi = interval
+    size = hi - lo
+    return lo + size * random.random()
 
-#input_value = sorted([get_random_interval_pt() for i in range(NUM_POINT)])
-input_value = [cheb_extrema(NUM_POINT, i) for i in range(NUM_POINT)]
-input_value = sorted(input_value)
-print("input_value={}".format(input_value))
 
-# as tanh is increasing we can get min/max
-# by looking at function value at interval bounds
-min_value = function(input_value[0])
-max_value = function(input_value[-1])
+def generate_approx(function, interval, NUM_POINT=100, poly_degree=4, epsilon=0.01, precision=53):
+    #input_value = sorted([get_random_interval_pt() for i in range(NUM_POINT)])
+    input_value = [cheb_extrema(NUM_POINT, i, interval) for i in range(NUM_POINT)]
+    input_value = sorted(input_value)
+    print("input_value={}".format(input_value))
 
-precision = 24
-factor = 2**precision / min_value
+    # as tanh is increasing we can get min/max
+    # by looking at function value at interval bounds
+    min_value = function(input_value[0])
+    max_value = function(input_value[-1])
 
-def int_conv(x):
-    return int(x * factor)
+    factor = 2**precision / min_value
 
-def back_conv(x):
-    return x / factor
+    # local conversion to/from integer functions
+    def int_conv(x):
+        return int(x * factor)
+    def back_conv(x):
+        return x / factor
 
-target_vector = [function(x) for i, x in enumerate(input_value)]
+    target_vector = [function(x) for i, x in enumerate(input_value)]
 
-matrix = fpylll.IntegerMatrix(poly_degree + 1, NUM_POINT)
-np_matrix = np.zeros((poly_degree + 1, NUM_POINT))
+    matrix = fpylll.IntegerMatrix(poly_degree + 1, NUM_POINT)
+    np_matrix = np.zeros((poly_degree + 1, NUM_POINT))
 
-# each column contains the i-th power of the input row
-for row in range(NUM_POINT):
-    for col in range(poly_degree+1):
-        coeff = int_conv(input_value[row]**col)
-        matrix[col, row] = coeff
-        np_matrix[col, row] = coeff
-print(matrix)
+    # each column contains the i-th power of the input row
+    for row in range(NUM_POINT):
+        for col in range(poly_degree+1):
+            coeff = int_conv(input_value[row]**col)
+            matrix[col, row] = coeff
+            np_matrix[col, row] = coeff
+    print(matrix)
 
-reduced_matrix = fpylll.IntegerMatrix(matrix)
+    reduced_matrix = fpylll.IntegerMatrix(matrix)
 
-fpylll.LLL.reduction(reduced_matrix)
-print(reduced_matrix)
+    # reducing matrix to simplify CVP search
+    fpylll.LLL.reduction(reduced_matrix)
+    print(reduced_matrix)
 
-conv_target = [int_conv(v) for v in target_vector]
-print("conv_target=", conv_target)
+    conv_target = [int_conv(v) for v in target_vector]
+    print("conv_target=", conv_target)
 
-closest_vector = fpylll.CVP.closest_vector(reduced_matrix, conv_target)
-print("closest vector: ", closest_vector)
-print("distance : ", back_conv(max(abs(a - b) for a, b in zip(closest_vector, conv_target))))
+    closest_vector = fpylll.CVP.closest_vector(reduced_matrix, conv_target)
+    print("closest vector: ", closest_vector)
+    print("distance : ", back_conv(max(abs(a - b) for a, b in zip(closest_vector, conv_target))))
 
-#poly_coeff = [back_conv(v) for v in closest_vector]
+    #poly_coeff = [back_conv(v) for v in closest_vector]
 
-b = np.array(closest_vector)
-M = np_matrix.transpose()
-print("M=", M)
-print("b=", b)
+    b = np.array(closest_vector)
+    M = np_matrix.transpose()
+    print("M=", M)
+    print("b=", b)
 
-poly_coeff = [v for v in np.linalg.lstsq(M, b)[0]]
-print("poly_coeff: ", poly_coeff)
+    poly_coeff = [v for v in np.linalg.lstsq(M, b)[0]]
+    print("poly_coeff: ", poly_coeff)
+
+    return poly_coeff
 
 def vector_eval(vector, value):
     acc = 0
@@ -108,36 +124,44 @@ def vector_eval(vector, value):
         acc += c * value**i
     return acc
 
-diff = max(vector_eval(poly_coeff, v) - function(v) for v in input_value)
-print("max_diff is {}".format(diff))
-NUM_TEST_PTS = 10
-print("testing on {} random points on the interval".format(NUM_TEST_PTS))
-for i in range(NUM_TEST_PTS):
-    pt = get_random_interval_pt()
-    diff = max(diff, vector_eval(poly_coeff, pt) - function(pt))
-print("max_diff is {}".format(diff))
+def eval_poly_vs_fct(poly_coeff, function, test_values):
+    diff = max(vector_eval(poly_coeff, v) - function(v) for v in test_values)
+    return diff
 
 
+if __name__ == "__main__":
+    func = lambda x: bigfloat.exp(x)
+    interval_lo, interval_hi = 0.01, 0.5
+    interval = interval_lo, interval_hi
+    # generating coefficients of polynomial approximation
+    poly_coeff = generate_approx(func, interval)
 
-# graphical representation
-fig = plt.figure()  # an empty figure with no axes
-fig.suptitle('No axes on this figure')  # Add a title so we know which it is
-
-x = np.linspace(interval_lo, interval_hi, 100)
-tanh_y = np.array([function(v) for v in x])
-poly_y = np.array([vector_eval(poly_coeff, v) for v in x])
-error_y = tanh_y - poly_y
+    # evaluating polynomial approximation on random points
+    NUM_TEST_PTS = 10
+    print("testing on {} random points on the interval".format(NUM_TEST_PTS))
+    max_diff = eval_poly_vs_fct(poly_coeff, func, (get_random_interval_pt(interval) for i in range(NUM_TEST_PTS)))
+    print("max_diff is {}".format(max_diff))
 
 
-plt.plot(x, tanh_y, label='tanh')
-plt.plot(x, poly_y, label='poly')
-plt.plot(x, error_y, label='error')
+    # graphical representation
+    fig = plt.figure()  # an empty figure with no axes
+    fig.suptitle('No axes on this figure')  # Add a title so we know which it is
 
-plt.title("Simple Plot")
+    x = np.linspace(interval_lo, interval_hi, 100)
+    tanh_y = np.array([func(v) for v in x])
+    poly_y = np.array([vector_eval(poly_coeff, v) for v in x])
+    error_y = tanh_y - poly_y
 
-plt.legend()
 
-plt.show()
+    plt.plot(x, tanh_y, label='tanh')
+    plt.plot(x, poly_y, label='poly')
+    plt.plot(x, error_y, label='error')
+
+    plt.title("Simple Plot")
+
+    plt.legend()
+
+    plt.show()
 
 
 
